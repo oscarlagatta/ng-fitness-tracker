cbe89cde32c51f5e8d674bb7bad11f08fc304288
Oscar Lagatta <oscarlagatta@gmail.com>
2020-01-17 22:47:58 +0000

    setting up material design

---
+++ src/app/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
+@NgModule({
+  declarations: [],
+  imports: [MatButtonModule, MatIconModule],
+  exports: [MatButtonModule, MatIconModule]
+})
+export class MaterialModule {}
