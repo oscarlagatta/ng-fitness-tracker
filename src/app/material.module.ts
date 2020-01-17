dea0bcf87b0535ea2e19087c72b208e8db969032
Oscar Lagatta <oscarlagatta@gmail.com>
2020-01-17 23:09:43 +0000

    signup form draft

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [],
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule]
+  imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule]
 })
 export class MaterialModule {}
