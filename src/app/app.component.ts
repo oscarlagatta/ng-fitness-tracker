12efc3d0a546949b40e919847d80d67b1dfcbdb3
Oscar Lagatta <oscarlagatta@gmail.com>
2020-01-21 11:03:47 +0000

    styling the sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   openSideNav = false;
-  title = 'fitness-tracker';
 }
