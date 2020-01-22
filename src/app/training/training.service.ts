d6c406ef413f5a380bf94eff044d2d1d01d677cc
Oscar Lagatta <oscarlagatta@gmail.com>
2020-01-22 23:31:59 +0000

    connecting real time to database

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -8,10 +8,10 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class TrainingService {
   exerciseChanged = new Subject<Exercise>();
   exercisesChanged = new Subject<Exercise[]>();
+  finishedExercisesChanged = new Subject<Exercise[]>();
 
   private availableExercises: Exercise[] = [];
   private runningExercise: Exercise;
-  private exercises: Exercise[] = [];
 
   constructor(private db: AngularFirestore) {}
 
@@ -66,8 +66,14 @@ export class TrainingService {
     return { ...this.runningExercise };
   }
 
-  getCompletedOrCancelledExercises() {
-    return this.exercises.slice();
+  fetchCompletedOrCancelledExercises() {
+    // return this.finishedExercises.slice();
+    this.db
+      .collection('finishedExercises')
+      .valueChanges()
+      .subscribe((exercises: Exercise[]) => {
+        this.finishedExercisesChanged.next(exercises);
+      });
   }
 
   private addDataToDatabase(exercise: Exercise) {
