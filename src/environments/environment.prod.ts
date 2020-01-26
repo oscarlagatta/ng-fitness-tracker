c96c9460ffc1bc2447eb889533887ea9c6ffc6fb
Oscar Lagatta <oscarlagatta@gmail.com>
2020-01-26 13:48:58 +0000

    corrects environment.prod file and docs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDOVICUPyjhBVa1ggwT7tgZJeIiXHxYmHs',
+    authDomain: 'ng-fitness-tracker-2adb7.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-2adb7.firebaseio.com',
+    projectId: 'ng-fitness-tracker-2adb7',
+    storageBucket: 'ng-fitness-tracker-2adb7.appspot.com',
+    messagingSenderId: '875481073541',
+    appId: '1:875481073541:web:5023d892fee396f9c6e6ec',
+    measurementId: 'G-JKCZCKPN7M'
+  }
 };
