ee6e0bb5c34f2742ba3ccce4cf87531b4f5d1b92
Oscar Lagatta <oscarlagatta@gmail.com>
2020-01-26 17:16:21 +0000

    selects training state in components

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
 import { NgForm } from '@angular/forms';
 import { Observable, Subscription, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { UIService } from 'src/app/shared/ui.service';
+import * as fromTraining from '../training.reducer';
 import * as fromRoot from '../../app.reducer';
+
 import { Store } from '@ngrx/store';
 
 @Component({
@@ -13,8 +15,8 @@ import { Store } from '@ngrx/store';
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.scss']
 })
-export class NewTrainingComponent implements OnInit, OnDestroy {
-  exercises: Exercise[];
+export class NewTrainingComponent implements OnInit {
+  exercises$: Observable<Exercise[]>;
   private exerciseSubscription: Subscription;
 
   isLoading$: Observable<boolean>;
@@ -22,35 +24,20 @@ export class NewTrainingComponent implements OnInit, OnDestroy {
   constructor(
     private trainingService: TrainingService,
     private uiService: UIService,
-    private store: Store<fromRoot.State>
+    private store: Store<fromTraining.State>
   ) {}
 
   ngOnInit() {
     this.isLoading$ = this.store.select(fromRoot.getIsLoading);
-    // this.exercises = this.trainingService.getAvailableExercises();
-    // this.loadingSubscription = this.uiService.loadingStateChanged.subscribe(
-    //   isLoading => {
-    //     this.isLoading = isLoading;
-    //   }
-    // );
-    this.exerciseSubscription = this.trainingService.exercisesChanged.subscribe(
-      exercises => {
-        this.exercises = exercises;
-      }
-    );
+    this.exercises$ = this.store.select(fromTraining.getAvailableExercises);
     this.fetchExercises();
   }
 
   fetchExercises() {
     this.trainingService.fetchAvailableExercises();
   }
+
   onStartTraining(form: NgForm) {
     this.trainingService.startExercise(form.value.exercise);
-  }
-
-  ngOnDestroy() {
-    if (this.exerciseSubscription) {
-      this.exerciseSubscription.unsubscribe();
-    }
   }
 }
--- src/app/training/training.reducer.ts
+++ src/app/training/training.reducer.ts
@@ -72,3 +72,7 @@ export const getActiveTraining = createSelector(
   getTrainingState,
   (state: TrainingState) => state.activeTraining
 );
+export const getIsTraining = createSelector(
+  getTrainingState,
+  (state: TrainingState) => state.activeTraining != null
+);
